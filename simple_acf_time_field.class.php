57edf5428da555610b5ae8bcd2997a8a676d12bf
Alex Mamut <amamut@northps.com>
2013-03-01 09:45:01 -0500

    Modified the way 12 hour format is saved on backend.

--- simple_acf_time_field.class.php
+++ simple_acf_time_field.class.php
@@ -8,14 +8,14 @@ class Simple_ACF_Time_Field extends acf_Field
 	function __construct($parent)	{
 		// do not delete!
     	parent::__construct($parent);
-    	
+
     	// set name / title
     	$this->name = 'simple_time_field'; // variable name (no spaces / special characters / etc)
       $this->title = __("Time",'acf'); // field label (Displayed in edit screens)
-		
+
    	}
 
-	
+
 	/**
    * Create field option form
    *
@@ -25,8 +25,8 @@ class Simple_ACF_Time_Field extends acf_Field
 		// defaults
 		$field['time_format'] = isset($field['time_format']) ? $field['time_format'] : '';
     $field['24_hour'] = isset($field['24_hour']) ? $field['24_hour'] : false;
-		
-		?>    
+
+		?>
     <tr class="field_option field_option_<?php echo $this->name; ?>">
       <td class="label">
         <label><?php _e('24 hour time'); ?></label>
@@ -47,22 +47,28 @@ class Simple_ACF_Time_Field extends acf_Field
 
 		<?php
 	}
-		
-	
-	
+
+
+
   /**
    * Output the post edit form for the field.
    */
 	function create_field($field)	{
 		// vars
-		$field['24_hour'] = isset($field['24_hour']) ? $field['24_hour'] : false;
-    $hour = floor($field['value']/60);
-    $minute = $field['value']%60;
-    
+    if ( isset($field['24_hour']) ) {
+      $hour = floor($field['value']/60);
+      $minute = $field['value']%60;
+    } else {
+      $hour = date('g', strtotime($field['value']));
+      $minute = date('i', strtotime($field['value']));
+      $meridiem = date('A', strtotime($field['value']));
+      $field['24_hour'] = false;
+    }
+
 		// html
     ?>
     <select name="<?php echo $field['name']; ?>[hour]">
-      <?php 
+      <?php
       $start = $field['24_hour'] ? 0 : 1;
       $end = $field['24_hour'] ? 23: 12;
       for($i=$start; $i<=$end; ++$i) {
@@ -71,22 +77,23 @@ class Simple_ACF_Time_Field extends acf_Field
     </select>
     :
     <select name="<?php echo $field['name']; ?>[minute]">
-      <?php 
+      <?php
       for($i=0; $i<=59; ++$i) {
-        printf( '<option value="%d" %s>%02d</option>', $i, ($minute==$i ? 'selected="selected"' : ''), $i);
+        $minutes = date('i', strtotime('12:' . $i));
+        echo '<option value="' . $minutes . '" ' . ($minute==$minutes ? 'selected="selected"' : '') . '>' . $minutes . '</option>';
       }?>
     </select>
     <?php if(!$field['24_hour']): ?>
     <select name="<?php echo $field['name']; ?>[meridiem]">
-      <option value="am" <?php if($hour<12) echo 'selected="selected"'; ?>>AM</option>
-      <option value="pm" <?php if($hour>=12) echo 'selected="selected"'; ?>>PM</option>
+      <option value="am" <?php echo $meridiem == 'AM' ? 'selected="selected"' : ''; ?>>AM</option>
+      <option value="pm" <?php echo $meridiem == 'PM' ? 'selected="selected"' : ''; ?>>PM</option>
     </select>
     <?php endif;
 	}
-	
-	
 
-	
+
+
+
   /**
    * Include stylesheet
    */
@@ -95,7 +102,7 @@ class Simple_ACF_Time_Field extends acf_Field
     wp_enqueue_style( 'simple-acf-time-field', $url);
 	}
 
-	
+
 	/**
    * Process the post edit submit
    *
@@ -108,49 +115,46 @@ class Simple_ACF_Time_Field extends acf_Field
     if($field['24_hour']) {
       $val = $value['hour'] * 60 + $value['minute'];
     } else {
-      $val = ($value['hour']%12) * 60 + $value['minute'];
-      if($value['meridiem'] == 'pm') {
-        $val += 12*30;
-      }
+      $val = $value['hour'] . ':' . $value['minute'] . ' ' . $value['meridiem'];
     }
-    
+
 		// save value
 		parent::update_value($post_id, $field, $val);
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/*--------------------------------------------------------------------------------------
 	*
 	*	get_value_for_api
-	*	- called from your template file when using the API functions (get_field, etc). 
+	*	- called from your template file when using the API functions (get_field, etc).
 	*	This function is useful if your field needs to format the returned value
 	*
 	*	@params
 	*	- $post_id (int) - the post ID which your value is attached to
 	*	- $field (array) - the field object.
-	* 
+	*
 	*-------------------------------------------------------------------------------------*/
-	
+
 	function get_value_for_api($post_id, $field){
 		// get value
 		$value = $this->get_value($post_id, $field);
-		
+
 		// format value
     $field['24_hour'] = isset($field['24_hour']) ? $field['24_hour'] : false;
 		$field['time_format'] = isset($field['time_format']) ? $field['time_format'] : $field['24_hour'] ? 'G:i' : 'g:ia';
-    
+
     $value = $this->format_minutes($value, $field['time_format']);
 		// return value
 		return $value;
 
 	}
-  
-  
-  
+
+
+
   /**
    * Format minutes into a time format
    *
@@ -162,7 +166,7 @@ class Simple_ACF_Time_Field extends acf_Field
    function format_minutes($minutes, $format) {
     $hour = floor($minutes/60);
     $min = $minutes%60;
-    
+
     $tokens = array(
       'a' => $hour < 12 ? 'am' : 'pm',
       'A' => $hour < 12 ? 'AM' : 'PM',
@@ -172,8 +176,8 @@ class Simple_ACF_Time_Field extends acf_Field
       'H' => sprintf('%02d', $hour),
       'i' => sprintf('%02d', $min),
     );
-    
+
     return str_replace(array_keys($tokens), $tokens, $format);
    }
-	
+
 }
